0d382fb677a6887ba3fdf777a5e15826cb65c53f
bobo <bisamov@gmail.com>
2019-01-18 23:14:24 -0600

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,17 +77,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'blog',
-#         'USER': 'bobo',
-#         'PASSWORD': 'simcards1.',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
